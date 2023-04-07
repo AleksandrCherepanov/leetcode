8e50299e36c69b343ea32a3900a0743ff1fd6ba2
Aleksandr Cherepanov <a.cherepanov@xsolla.com>
2023-04-07 11:54:05 +0800

    some problems & reorganize

---
+++ 300_400/328_odd_even_linked_list.go
@@ -0,0 +1,23 @@
+package main
+
+func oddEvenList(head *ListNode) *ListNode {
+    if head == nil || head.Next == nil {
+        return head
+    }
+
+    odd := head
+    even := odd.Next
+
+    for even != nil && even.Next != nil {        
+        oNext := odd.Next
+        odd.Next = even.Next
+        even.Next = even.Next.Next
+        odd.Next.Next = oNext
+        
+        odd = odd.Next
+        even = even.Next
+    }
+
+    return head
+}
+
