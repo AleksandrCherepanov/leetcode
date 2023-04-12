0b0ec51c448b393114a5209b3b7eab8a76db6e02
Aleksandr Cherepanov <a.cherepanov@xsolla.com>
2023-04-12 10:08:17 +0800

    some problems

---
+++ 2000_2100/2074_reverse_nodes_in_even_length_groups.go
@@ -0,0 +1,54 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func reverseEvenLengthGroups(head *ListNode) *ListNode {
+	dummy := &ListNode{0, head}
+	prev := dummy
+	var nextHead *ListNode
+
+	for i := 1; i < 1e5 && head != nil; i++ {
+		tail := head
+
+		j := 1
+		for(j < i && tail != nil && tail.Next != nil) {
+			tail = tail.Next
+			j++
+		}
+
+		nextHead = tail.Next
+
+		if j % 2 == 0 {
+			tail.Next = nil
+			prev.Next = reverse(head)
+			prev = head
+			head.Next = nextHead
+			head = nextHead
+		} else {
+			prev = tail
+			head = nextHead
+		}
+	}
+
+	return dummy.Next
+}
+
+func reverse(head *ListNode) *ListNode {
+	if head == nil {
+		return head
+	}
+
+	prev := &ListNode{0, nil}
+
+	for head != nil {
+		nxt := head.Next
+		head.Next = prev
+		prev = head
+		head = nxt
+	}
+
+	return prev
+}
--- main.go
+++ main.go
@@ -1,38 +1,4 @@
 package main
 
-import "github.com/AleksandrCherepanov/leetcode/structure"
-
 func main() {
-	root := structure.NewTreeNode(
-		1,
-		structure.NewTreeNode(
-			2,
-			structure.NewTreeNode(
-				3,
-				nil,
-				nil,
-			),
-			structure.NewTreeNode(
-				4,
-				structure.NewTreeNode(5, nil, nil),
-				structure.NewTreeNode(6, nil, nil),
-			),
-		),
-		structure.NewTreeNode(
-			7,
-			nil,
-			structure.NewTreeNode(
-				8,
-				structure.NewTreeNode(
-					9,
-					nil,
-					nil,
-				),
-				nil,
-			),
-		),
-	)
-
-	root.PrintPreOrder()
-	root.PrintInOrder()
 }
